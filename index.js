fc8a916bac6acf474cf3614ada05e700de0f4714
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-06-06 15:29:21 +0300

    commit tree 8e24c2f0fff6388f6daf91887f4932ea730d886d

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31;
+let num2 =2;
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10 + 1)
+
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+const max = Math.max(3, 9, 20)
+
